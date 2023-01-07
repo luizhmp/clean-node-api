389995edf7da83d6a8451c508c9716c23cbfa199
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-01-07 13:05:15 -0300

    ensures AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -20,7 +20,11 @@ class AuthUseCase {
       throw new InvalidParamError("loadUserByEmailRepository");
     }
 
-    await this.loadUserByEmailRepository.load(email);
+    const user = await this.loadUserByEmailRepository.load(email);
+
+    if (!user) {
+      return null;
+    }
   }
 }
 
@@ -78,5 +82,15 @@ describe("Auth Usecase", () => {
     expect(promise).rejects.toThrow(
       new InvalidParamError("loadUserByEmailRepository")
     );
+  });
+
+  test("Should return null if LoadUserByEmailRepository returns null", async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.auth(
+      "invalid_email@email.com",
+      "any_password"
+    );
+
+    expect(accessToken).toBeNull();
   });
 });
