6967652e4183b360c2dba643148b25714031c0d2
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-01-07 13:27:48 -0300

    removes unnecessary ifs

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,5 +1,5 @@
 const AuthUseCase = require("./auth-usecase");
-const { InvalidParamError, MissingParamError } = require("../../utils/errors");
+const { MissingParamError } = require("../../utils/errors");
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
@@ -43,18 +43,14 @@ describe("Auth Usecase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth("any_email@email.com", "any_password");
 
-    expect(promise).rejects.toThrow(
-      new MissingParamError("loadUserByEmailRepository")
-    );
+    expect(promise).rejects.toThrow();
   });
 
   test("Should throw if no LoadUserByEmailRepository has no load method", async () => {
     const sut = new AuthUseCase({});
     const promise = sut.auth("any_email@email.com", "any_password");
 
-    expect(promise).rejects.toThrow(
-      new InvalidParamError("loadUserByEmailRepository")
-    );
+    expect(promise).rejects.toThrow();
   });
 
   test("Should return null if LoadUserByEmailRepository returns null", async () => {
